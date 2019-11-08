602c5048978cf720a4edeb7d05c87f990ef07aa2
Emily Fischer <emily.m.fischer@outlook.com>
2019-11-08 16:37:54 -0600

    update with graph without state labels

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -23,12 +23,12 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("../data/data.csv")
+d3.csv("./assets/data/data.csv")
     .then(function (healthData) {
         // Step 1: Parse Data/Cast as numbers
         // ==============================
         healthData.forEach(function (data) {
-            data.state = +data.state;
+            // data.state = +data.state;
             data.abbr = +data.abbr ;
             data.poverty = +data.poverty;
             data.povertyMoe = +data.povertyMoe;
@@ -51,7 +51,7 @@ d3.csv("../data/data.csv")
         // Step 2: Create scale functions
         // ==============================
         var xLinearScale = d3.scaleLinear()
-            .domain([20, d3.max(healthData, d => d.poverty)])
+            .domain([8, d3.max(healthData, d => d.poverty)])
             .range([0, width]);
 
         var yLinearScale = d3.scaleLinear()
@@ -80,44 +80,45 @@ d3.csv("../data/data.csv")
             .append("circle")
             .attr("cx", d => xLinearScale(d.poverty))
             .attr("cy", d => yLinearScale(d.healthcare))
-            .attr("r", "15")
+            .attr("r", "10")
             .attr("fill", "blue")
+            // .text(d.state)
             .attr("opacity", ".5");
 
-    //     // Step 6: Initialize tool tip
-    //     // ==============================
-    //     var toolTip = d3.tip()
-    //         .attr("class", "tooltip")
-    //         .offset([80, -60])
-    //         .html(function (d) {
-    //             return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
-    //         });
+        // Step 6: Initialize tool tip
+        // ==============================
+        var toolTip = d3.tip()
+            .attr("class", "tooltip")
+            .offset([80, -60])
+            .html(function (d) {
+                return (`${d.state}<br>Poverty: ${d.poverty}%<br>Lacks Healthcare: ${d.healthcare}%`);
+            });
 
-    //     // Step 7: Create tooltip in the chart
-    //     // ==============================
-    //     chartGroup.call(toolTip);
+        // Step 7: Create tooltip in the chart
+        // ==============================
+        chartGroup.call(toolTip);
 
-    //     // Step 8: Create event listeners to display and hide the tooltip
-    //     // ==============================
-    //     circlesGroup.on("click", function (data) {
-    //         toolTip.show(data, this);
-    //     })
-    //         // onmouseout event
-    //         .on("mouseout", function (data, index) {
-    //             toolTip.hide(data);
-    //         });
+        // Step 8: Create event listeners to display and hide the tooltip
+        // ==============================
+        circlesGroup.on("click", function (data) {
+            toolTip.show(data, this);
+        })
+            // onmouseout event
+            .on("mouseout", function (data, index) {
+                toolTip.hide(data);
+            });
 
-    //     // Create axes labels
-    //     chartGroup.append("text")
-    //         .attr("transform", "rotate(-90)")
-    //         .attr("y", 0 - margin.left + 40)
-    //         .attr("x", 0 - (height / 2))
-    //         .attr("dy", "1em")
-    //         .attr("class", "axisText")
-    //         .text("Number of Billboard 100 Hits");
+        // Create axes labels
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left + 40)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .attr("class", "axisText")
+            .text("Lacks Healthcare (%)");
 
-    //     chartGroup.append("text")
-    //         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    //         .attr("class", "axisText")
-    //         .text("Hair Metal Band Hair Length (inches)");
+        chartGroup.append("text")
+            .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+            .attr("class", "axisText")
+            .text("In Poverty (%)");
     });
